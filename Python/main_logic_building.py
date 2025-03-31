41160fff9a029743e7e3b3773f9ef274486fb17b
DanKair <kair.daniyaruyl@gmail.com>
2025-03-31 15:45:03 +0500

    Factorial implementation using for loop added; Raise user entered number by user entered exponent program added

--- Python/main_logic_building.py
+++ Python/main_logic_building.py
@@ -13,4 +13,25 @@ while iterator != num:
 print(f"The factorial of {num} is {factorial}")
 
 
+# Factorial Program using for loop
+
+n = int(input("Enter the number of your choice: "))
+result = 1
+# Why n+1? Because n is excluded and the range() behaves like this without n+1 (int i = 1; i < n; i++)
+# That's why we wanna actually it to be like this: (int i = 1; i <= n; i++), to achieve this behavior, we just made n+1
+for i in range(1, n+1): 
+    result *= i
+    print(f"The factorial of {i}! = {result}")
+
+
+# Raise the user entered number to user entered exponent
+
+"""x = int(input("Enter the base number: "))
+y = int(input("Enter the exponent: "))
+def raise_by_exponent(a, b):
+    result = a**b
+    return f"The {a} raised by {b} = {result}"
+
+print(raise_by_exponent(x, y))"""
+
 
