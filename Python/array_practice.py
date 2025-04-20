5658049648e40918b2158ca7c560b115ac06a078
DanKair <kair.daniyaruyl@gmail.com>
2025-04-20 19:51:03 +0500

    Two sum problem solved

--- Python/array_practice.py
+++ Python/array_practice.py
@@ -13,6 +13,25 @@ while list != sorted(list):
             list[i], list[i+1] = list[i+1], list[i]
             print(list)
 
+# Two Sum Problem
+# 1. Find the target value
+# 2. Traverse through the list
+# 3. if sum == target: print(indeces)
+# 4. sum should be addition of two elements together, but expect themself
+# 5. each element should be checked with each other (like 0 index with 1-3 indexes, 1 index with 0, 2 and 3 indexes and etc)
+nums = [2, 7, 11, 15]
+target = 9
+indexes = []
+
+for i in range(len(nums)):
+    sum = 0
+    for j in range(i + 1, len(nums)): # i + 1 avoids comparing same pairs together (2,7 with 7,2 )
+        sum = nums[i] + nums[j]
+        print(f"Sum of {nums[i]} and {nums[j]} = {sum}")
+        if sum == target:
+            indexes.append(i)
+            indexes.append(j)
+print(f"The indexes are: {indexes}")
 
 
         
